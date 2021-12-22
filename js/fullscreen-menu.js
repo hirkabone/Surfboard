aaa3c65fd88c6f1aad3db1ae5dbc1d393afcfda4
Egor Ganichev <zhora.feral@mail.ru>
2021-12-22 15:05:50 +0300

    amended

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -4,8 +4,10 @@ const closingBtn = document.querySelector(".fullscreen-menu__close");
 
 openingBtn.addEventListener('click', function() {
     menu.classList.add("active");
+    document.body.style.overflow = 'hidden';
 });
 
 closingBtn.addEventListener('click', function() {
     menu.classList.remove("active");
+    document.body.style.overflow = 'visible';
 });
