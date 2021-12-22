3d624b1740b09542a06700497cb00ac8c791d28e
Egor Ganichev <zhora.feral@mail.ru>
2021-12-22 20:40:45 +0300

    changed

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -1,13 +1,14 @@
 const openingBtn = document.querySelector(".hamburger");
 const menu = document.querySelector(".fullscreen-menu");
 const closingBtn = document.querySelector(".fullscreen-menu__close");
+const body = document.querySelector("body");
 
 openingBtn.addEventListener('click', function() {
     menu.classList.add("active");
-    document.body.style.overflow = 'hidden';
+    body.classList.add("hidden");
 });
 
 closingBtn.addEventListener('click', function() {
     menu.classList.remove("active");
-    document.body.style.overflow = 'visible';
+    body.classList.remove("hidden");
 });
